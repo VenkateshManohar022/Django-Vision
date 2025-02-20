ff7ddca7c69a464fde74532980e0a9aa460db157
Venkatesh Manohar <manohar@Venkateshs-MacBook-Pro.local>
2025-02-20 14:21:01 +0530

    Harrcascade Update Lines

--- App1/utils.py
+++ App1/utils.py
@@ -14,6 +14,7 @@ def selectionChoices(image,choice):
     elif choice == 'FACE':
         data = ""
         face_classifier = cv2.CascadeClassifier(os.path.join(settings.STATICFILES_DIRS[0],'Haarcascades/haarcascade_frontalface_default.xml'))
+        # face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         faces = face_classifier.detectMultiScale(gray, 1.3, 5)
